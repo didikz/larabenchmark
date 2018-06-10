0d943456e789f83590306d6a67a5776539216255
Didik Tri Susanto <rezd14@gmail.com>
2018-06-10 08:56:21 +0700

    increase expired

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function index(User $user)
     {
-        $data = Cache::remember('users', 60, function () use ($user) {
+        $data = Cache::remember('users', 22*60, function () use ($user) {
             $data['user_count'] = $user->count();
             $data['users'] = $user->orderBy('email')->get();
             \Log::info('caching');
